1b947c783e13da221a3d51a3c81ae0aa52e5ec1c
wzwzx <69845256+wzwzx@users.noreply.github.com>
2022-07-31 12:14:05 +0800

    begonia: Build Kaleidoscope overlay

--- device.mk
+++ device.mk
@@ -176,7 +176,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-kscope
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
