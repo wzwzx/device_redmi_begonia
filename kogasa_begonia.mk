747a90f9ed77fa193501c5d3c97aa1f8144a0a13
wzwzx <antidooo@163.com>
2022-10-01 13:12:03 +0000

    begonia: KogasaKyokai bringup

--- kogasa_begonia.mk
+++ kogasa_begonia.mk
@@ -28,11 +28,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 $(call inherit-product, device/redmi/begonia/device.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+$(call inherit-product, vendor/kogasa/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := begonia
-PRODUCT_NAME := potato_begonia
+PRODUCT_NAME := kogasa_begonia
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
